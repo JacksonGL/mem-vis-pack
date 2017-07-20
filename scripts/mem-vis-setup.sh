7f76bc5ace26a2718618692ad1cd31572c9d5b78
JacksonGL <jacksongl1988@gmail.com>
2017-07-19 18:46:37 -0700

    update script

--- scripts/mem-vis-setup.sh
+++ scripts/mem-vis-setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # install node-chakracore via nvs
-nvs remote mem-analysis https://raw.githubusercontent.com/JacksonGL/node-chakracore/AllocTracing/releases
-nvs use mem-analysis
+export NVS_HOME="$HOME/.nvs"
+. "$NVS_HOME/nvs.sh" install
+
+nvs remote mem-vis https://raw.githubusercontent.com/JacksonGL/node-chakracore/AllocTracing/releases
+
+nvs add mem-vis/8.0.0
+# nvs use mem-vis/8.0.0
 
 # install the mem-vis-tool
 git clone https://github.com/JacksonGL/mem-vis-pack.git
