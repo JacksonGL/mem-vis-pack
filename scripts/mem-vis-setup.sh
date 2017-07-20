dc30d71bc3526d856d28c80eecad05509d03f9aa
JacksonGL <jacksongl1988@gmail.com>
2017-07-19 19:17:10 -0700

    update install script

--- scripts/mem-vis-setup.sh
+++ scripts/mem-vis-setup.sh
@@ -10,6 +10,6 @@ nvs add mem-vis/8.0.0
 # nvs use mem-vis/8.0.0
 
 # install the mem-vis-tool
-git clone https://github.com/JacksonGL/mem-vis-pack.git
-cd mem-vis-pack
+# git clone https://github.com/JacksonGL/mem-vis-pack.git
+# cd mem-vis-pack
 npm install
