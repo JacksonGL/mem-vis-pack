469be4ef36b1603fd3f1a39e98c11dbb7f92fe31
Liang Gong <JacksonGL@users.noreply.github.com>
2017-09-03 19:11:41 -0700

    update doc

--- scripts/mem-vis-setup.sh
+++ scripts/mem-vis-setup.sh
@@ -5,11 +5,4 @@ export NVS_HOME="$HOME/.nvs"
 . "$NVS_HOME/nvs.sh" install
 
 nvs remote mem-vis https://raw.githubusercontent.com/JacksonGL/node-chakracore/AllocTracing/releases
-
 nvs add mem-vis/9.0.0
-# nvs use mem-vis/8.0.0
-
-# install the mem-vis-tool
-# git clone https://github.com/JacksonGL/mem-vis-pack.git
-# cd mem-vis-pack
-npm install
